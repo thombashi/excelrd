3d081485e00dc15f50acd2360c350d892fc51b76
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2020-01-05 14:47:22 +0900

    Drop Python 2 support

--- setup.py
+++ setup.py
@@ -30,8 +30,6 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
@@ -41,5 +39,5 @@ setup(
         'Topic :: Office/Business',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
-    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
+    python_requires=">=3.5",
 )
